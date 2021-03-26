1ee5e82a7df6b437e878b82b1471b93dfa5373ea
DrPotatoV <a1231124@uabc.edu.mx>
2021-03-25 18:57:58 -0700

    Adaptando el codigo del prof

--- hangman.py
+++ hangman.py
@@ -1,11 +1,11 @@
 # Import modules to use
-from words import palabras
+from words import palabras, vidasPics
 
 from colorama import Fore, Back, Style #Importar colorama
 import random
 
 # 1. Bienvenida
-print(Fore.GREEN + "Welcome to the game hangman in Python")
+print(Fore.GREEN + "Bienvenido al juego del Ahorcado")
 print(Fore.RED + "Comenzaras con 6 vidas, mucha suerte!")
 print(Style.RESET_ALL)
 vidas = 6
@@ -34,6 +34,46 @@ for i in range(largpalab):
 print(Fore.GREEN + guiones)
 print(Style.RESET_ALL)
 
+def hangman(): #Mecanica del juego
+
+  word = get_valid_word(palabras) # obtener palabra
+  word_letters = set(word) # Separar palabra, ej: S, O , L
+  alphabet = set(string.ascii_uppercase) #Convertir a mayus A, B, C, D, E,...
+  letras_usadas = set()
+  vidas = 7
+
+while len(word_letters) > 0 and vidas > 0:
+    print(f"Te quedan {vidas} vidas restantes y ya has usado estas letras: ",' '.join(letras_usadas))
+
+    # what letter you guess, example WORD (- - R D)
+    word_list = [letter if letter in letras_usadas else '-' for letter in word]
+
+    print("Current word: ", ' '.join(word_list),"\n")
+
+    user_letter = input('Guess a letter:').upper() # S
+
+    if user_letter in alphabet - letras_usadas: # si "S" esta en "A,B,C,D" - ""
+      letras_usadas.add(user_letter) # S
+      # print(letras_usadas,'\n',word_letters)
+
+      if user_letter in word_letters: # S es parte de tu palabra
+        word_letters.remove(user_letter) # S, O , L le quito la S = O, L
+      else:
+        vidas = vidas - 1
+        print("Esta letra no está en la palabra")
+
+    elif user_letter in letras_usadas:
+      print("Ya usaste esta palabra, intenta con otra.")
+    else:
+      print("Caracter invalido. Usa puras letras.")
+
+  if vidas == 0
+    print("Has perdido, la palabra era: ", word)
+  else:
+    print("Atinaste: ", word,"!!! =)")
+
+
+hangman()
 
 
 
